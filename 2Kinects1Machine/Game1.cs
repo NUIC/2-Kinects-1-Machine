46c68f46e933bc22a7f5e07a9a6668b14086d851
Evan Kochuk <ewkochuk@ncsu.edu>
2012-02-09 17:23:45 -0500

    I changed the color to something longer.

--- 2Kinects1Machine/Game1.cs
+++ 2Kinects1Machine/Game1.cs
@@ -81,7 +81,8 @@ namespace _2Kinects1Machine
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            //black is lame. Let's use something longer. LightGoldenrodYellow
+            GraphicsDevice.Clear(Color.LightGoldenrodYellow);
 
             // TODO: Add your drawing code here
 
