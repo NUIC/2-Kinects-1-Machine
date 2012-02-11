499a7b3e15d1225c55fdbb2b1633a6c607e9f454
Daniel Kennedy <danwkennedy@gmail.com>
2012-02-11 03:37:20 -0500

    KinectClient now sends skeleton data over an anonymous pipe. Need to work on setting up the main server now.

--- KinectClient/Program.cs
+++ KinectClient/Program.cs
@@ -6,22 +6,41 @@ using System.IO;
 using System.IO.Pipes;
 using System.Diagnostics;
 using System.Threading;
+using Microsoft.Kinect;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 
 namespace KinectClient
 {
     class KinectClient
     {
+        // The client pipestream for sending SkeletonData to the parent
+        private static PipeStream clientStream;
+        // Store a reference to this process' Kinect sensor
+        private static KinectSensor sensor;
+
+        // Smoothing values for the skeleton tracker
+        private static TransformSmoothParameters smoothingParameters = new TransformSmoothParameters
+        {
+            Smoothing = 0.1f,
+            Correction = 0.0f,
+            Prediction = 0.0f,
+            JitterRadius = 1.0f,
+            MaxDeviationRadius = 0.5f
+        };
+
+
         static void Main(string[] args)
         {
-#if (DEBUG)
-            if (args.Length == 0)
-            {
 
-                
+#if (DEBUG)
+            if (args.Length != 2)
+            {
                 SetupParentProcess();
             }
 #else
-            if (args.Length == 0)
+            // We are expecting exactly two arguments (both are required)
+            if (args.Length != 2)
             {
                 throw new Exception("No pipe data passed");
             }
@@ -30,41 +49,110 @@ namespace KinectClient
             try
             {
                 Console.WriteLine("[Client] Starting pipe stream");
-                PipeStream clientStream = new AnonymousPipeClientStream(PipeDirection.Out, args[0]);
+                clientStream = new AnonymousPipeClientStream(PipeDirection.Out, args[0]);
                 
                 Console.WriteLine("[Client] Stream opened");
-                Console.WriteLine("Transmission mode: {0}", clientStream.TransmissionMode.ToString());
+                //Console.WriteLine("Transmission mode: {0}", clientStream.TransmissionMode.ToString());
 
-                
-                StreamWriter writer = new StreamWriter(clientStream);
+                sensor = null;
 
-                ConsoleKey key;
-                while ((key = Console.ReadKey().Key)!= ConsoleKey.Escape)
+                Console.WriteLine("[Client] Found {0} Kinects", KinectSensor.KinectSensors.Count);
+
+                /**
+                 * Setting up the Kinect
+                 **/
+                if (KinectSensor.KinectSensors.Count > 0)
                 {
-                    if (key == ConsoleKey.Enter)
-                    {
-                        writer.Write((char)13);
-                        writer.Flush();
-                        clientStream.WaitForPipeDrain();
-                        Console.WriteLine("[Client] Pipe drained.");
-                        Console.WriteLine("\nWrite Something else");
-                    }
-                    else
-                    {
-                        string c = key.ToString();
-                        writer.Write(c);
-                    }
-                }
+                    KinectSensorCollection sensors = KinectSensor.KinectSensors;
+                    Console.WriteLine(args[1]);
+                    int sensorIndex = GetSensorIndexFromID(args[1]);
+                    
 
+                    sensor = KinectSensor.KinectSensors[sensorIndex];
+                    sensor.SkeletonStream.Enable(smoothingParameters);
+                    sensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(KinectSkeletonFrameReady);
+                    KinectSensor.KinectSensors.StatusChanged += new EventHandler<StatusChangedEventArgs>(KinectStatusChange);
+                    sensor.Start();
+                    while (clientStream.IsConnected) ;
+                }
+                else
+                {
+                    //throw new Exception("No Kinect sensors found");
+                    Console.WriteLine("No Kinect sensors found");
+                    while (true) ;
+                }
             }
             catch (Exception e)
             {
-                Console.WriteLine("[Client] Exception:\n    {0}", e.Message);
+                Console.WriteLine("[Client] Exception:\n    {0}\nTrace: {1}", e.Message, e.StackTrace);
             }
         }
 
-#if (DEBUG)
+        /// <summary>
+        /// Checks the inputted KinectID and returns its index in the KinectSensorCollection
+        /// </summary>
+        /// <param name="uniqueId">The unique Kinect Id inputed by the parent process</param>
+        /// <returns>The index into the KinectSensorCollection associated with the unique Id</returns>
+        static int GetSensorIndexFromID(string uniqueId)
+        {
+            for (int i = 0; i < KinectSensor.KinectSensors.Count; i++)
+            {
+                // Check to see if the Kinect is connected to the system
+                // Check to see if the Kinect is not already running
+                // Check to see if the UniqueId matches
+                // If all are true return
+                if (KinectSensor.KinectSensors[i].Status == KinectStatus.Connected 
+                    && !KinectSensor.KinectSensors[i].IsRunning
+                    && KinectSensor.KinectSensors[i].UniqueKinectId.Equals(uniqueId))
+                {
+                    return i;
+                }
+            }
 
+            throw new Exception("Invalid unique ID");
+        }
+
+        /// <summary>
+        /// The event handler for the SkeletonFrameReady event.
+        /// Grabs the skeleton data, serializes it and sends it over the pipe to the server
+        /// </summary>
+        /// <param name="sender">The sending object</param>
+        /// <param name="e">Contains the skeleton frame data</param>
+        static void KinectSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Console.WriteLine("[Client] Current Kinect frame: {0}", e.OpenSkeletonFrame().FrameNumber);
+            // If the previous frame is still being read, wait for it to finish
+            clientStream.WaitForPipeDrain();
+
+            // Grab the skeleton data and save it locally
+            Skeleton[] skeletonData = new Skeleton[e.OpenSkeletonFrame().SkeletonArrayLength];
+            e.OpenSkeletonFrame().CopySkeletonDataTo(skeletonData);
+
+            // Binary serialize and write the skeleton data over the pipe
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(clientStream, skeletonData);
+        }
+
+        /// <summary>
+        /// A Debug function that gets called whenever the Kinect's state change. Since a state change 
+        /// is usually a bad thing, this event handler will mainly print out diagnostic data.
+        /// </summary>
+        /// <param name="sender">The sending object</param>
+        /// <param name="e">Contains data on the sensor</param>
+        static void KinectStatusChange(object sender, StatusChangedEventArgs e)
+        {
+            Console.WriteLine("[Client] Kinect's status changed!");
+            Console.WriteLine("[Client] Kinect is running: {0}", e.Sensor.IsRunning);
+            Console.WriteLine("[Client] Kinect Skeletal stream is enabled: {0}", e.Sensor.SkeletonStream.IsEnabled);
+            Console.WriteLine("[Client] Kinect status: {0}", e.Sensor.Status);
+            Console.WriteLine("[Client] Kinect device connection ID: {0}", e.Sensor.DeviceConnectionId);
+        }
+
+#if (DEBUG)
+        /// <summary>
+        /// This method should be moved into MultKinectServer.cs.
+        /// Right now, this is easier for debugging and getting the client right.
+        /// </summary>
         static void SetupParentProcess()
         {
             AnonymousPipeServerStream pipeServer =
@@ -74,34 +162,38 @@ namespace KinectClient
             Process childProcess = new Process();
             childProcess.StartInfo.FileName = "D:\\git\\2KinectTechDemo\\KinectClient\\bin\\Debug\\KinectClient.exe";
             childProcess.StartInfo.Arguments += " " + pipeServer.GetClientHandleAsString();
-            childProcess.StartInfo.Arguments += " " + 0;
+            childProcess.StartInfo.Arguments += " " + KinectSensor.KinectSensors[0].UniqueKinectId;
+
+            Console.WriteLine("Setting up Kinect with ID: {0}", KinectSensor.KinectSensors[0].UniqueKinectId);
 
             childProcess.StartInfo.UseShellExecute = false;
             childProcess.Start();
 
             pipeServer.DisposeLocalCopyOfClientHandle();
 
+            Skeleton[] skeletonData;
+
             try
             {
                 // Read user input and send that to the client process.
                 using (StreamReader sr = new StreamReader(pipeServer))
                 {
-                    char temp;
-                    string message = "";
-                    Console.WriteLine("[Server] Waiting for input");
+                    //char temp;
+                    //string message = "";
+                    Console.WriteLine("[Server] Waiting for skeleton data");
 
                     while (pipeServer.IsConnected)
                     {
-                        temp = (char) sr.Read();
+                        BinaryFormatter formatter = new BinaryFormatter();
 
-                        if (temp != (char)13)
+                        try
                         {
-                            message += temp;
+                            skeletonData = (Skeleton[])formatter.Deserialize(pipeServer);
+                            Console.WriteLine("[Server] Found a skeleton with ID: {0}", skeletonData[0].TrackingId);
                         }
-                        else
+                        catch (SerializationException e)
                         {
-                            Console.WriteLine("[Server] Echo: {0} ", message);
-                            message = "";
+                            Console.WriteLine("[Server] Exception: {0}", e.Message);
                         }
                     }
                 }
@@ -111,7 +203,9 @@ namespace KinectClient
             catch (IOException e)
             {
                 Console.WriteLine("[SERVER-Thread1] Error: {0}", e.Message);
+                while (true) ;
             }
+            while(true);
         }
 
 #endif
